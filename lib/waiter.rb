cf91cbbdf6555da9f08cd0da6e721ef2bfe4d431
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-14 17:02:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :years_of_experience
+  @@all = []
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @@all << self
+    @meals = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    @customer = customer
+    @total = total
+    @tip = tip
+    @new_meal = Meal.new(self, @customer, @total, @tip)
+  end
+
+
+  def waiters
+    
+  end
+
 
 end
